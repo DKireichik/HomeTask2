601feeb655dd971d5d38dc3228955318d83371d9
Darya Kireychik <dsilchencko@gmail.com>
2023-10-09 22:58:55 +0300

    Initial Commit

---
+++ HomeTask2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeTask2
+//
+//  Created by Darya on 9.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
